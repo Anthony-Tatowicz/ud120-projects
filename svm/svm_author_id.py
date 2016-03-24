722ace9aa242bd8b598ba34be1b516928fe21882
Anthony Tatowicz <ajtatowicz@gmail.com>
2016-03-23 23:03:49 -0500

    naive_bays & svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel='rbf', C=10000.)
+
+t0 = time()
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
+clf.fit(features_train, labels_train)
+print("training time: ", round(time() - t0, 3),  's')
+
+t1 = time()
+pred = clf.predict(features_test)
+print("predict time: ", round(time() - t1, 3), 's')
+
+count = 0
+for i in range(len(pred)):
+    if pred[i] == 1:
+        count += 1
+
+print(count)
+# print(pred[10], pred[26], pred[50])
+print(accuracy_score(pred, labels_test))
 #########################################################
-
-
