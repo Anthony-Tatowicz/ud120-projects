df85221006fb41e4b9bf1e885fed9c76acd70f27
Anthony Tatowicz <ajtatowicz@gmail.com>
2016-05-10 18:09:47 -0500

    Validation
    Used cross validation to validate model performace

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,11 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import cross_validation
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -25,8 +29,15 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        features, labels, test_size=0.3, random_state=42)
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward!
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
 
+pred = clf.predict(X_test)
 
+score = accuracy_score(pred, y_test)
+print('Accuracy: {}'.format(score))
