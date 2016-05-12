97e383e6208e9ab44164b39b55dfeadcb6aa99bc
Anthony Tatowicz <ajtatowicz@gmail.com>
2016-05-12 01:23:21 -0500

    evaluation
    implemented evaulation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -25,7 +25,35 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### your code goes here
+from sklearn import cross_validation
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        features, labels, test_size=0.3, random_state=42)
 
-### your code goes here 
 
+### it's all yours from here forward!
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
 
+pred = clf.predict(X_test)
+
+num_pois = len([x for x in pred if x == 1])
+print('Number of pois {}'.format(num_pois))
+
+print('Number of test samples {}'.format(len(X_test)))
+
+score = accuracy_score(pred, y_test)
+print('Accuracy: {}'.format(score))
+
+true_pos = [x for i,x in enumerate(pred)  if x == y_test[i]]
+print(true_pos)
+
+p_score = precision_score(y_test, pred)
+print('precision_score : {}'.format(p_score))
+
+r_score = recall_score(y_test, pred)
+print('recall_score : {}'.format(r_score))
